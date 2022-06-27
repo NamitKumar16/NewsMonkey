b514d867367bd53dba44c1dc656839ef5b84932a
NamitKumar16 <kumarnamit58@gmail.com>
2022-06-27 23:34:37 +0530

    Fetching news from API

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,15 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              !imageUrl
+                ? "https://images.hindustantimes.com/img/2022/06/27/550x309/WhatsApp_Image_2021-09-18_at_09.42.18_1631944439782_1656288913805.jpeg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>

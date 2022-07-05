239b9127546d32aeaf594499ab095aefc0ba7a1c
NamitKumar16 <kumarnamit58@gmail.com>
2022-07-05 22:23:22 +0530

    Categories Added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,102 @@ import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
-  c = "John";
   render() {
     return (
       <div>
-        <Navbar />
-        <News pageSize={5} country="in" category="science" />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <News
+                  key="general"
+                  pageSize={5}
+                  country="in"
+                  category="general"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/business"
+              element={
+                <News
+                  key="business"
+                  pageSize={5}
+                  country="in"
+                  category="business"
+                />
+              }
+            />
+
+            <Route
+              exact
+              path="/entertainment"
+              element={
+                <News
+                  pageSize={5}
+                  key="entertainment"
+                  country="in"
+                  category="entertainment"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/health"
+              element={
+                <News
+                  pageSize={5}
+                  key="health"
+                  country="in"
+                  category="health"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/science"
+              element={
+                <News
+                  pageSize={5}
+                  key="science"
+                  country="in"
+                  category="science"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/sports"
+              element={
+                <News
+                  key="sports"
+                  pageSize={5}
+                  country="in"
+                  category="sports"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/technology"
+              element={
+                <News
+                  key="technology"
+                  pageSize={5}
+                  country="in"
+                  category="technology"
+                />
+              }
+            ></Route>
+          </Routes>
+        </Router>
       </div>
     );
   }
